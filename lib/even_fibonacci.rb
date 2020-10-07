8836917e136c88fa815fe3c5389467c26f4b5020
slee713 <steven.lee1393@gmail.com>
2020-10-07 10:12:16 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+    array = [1,2]
+    while array[-1] < limit 
+      new_value = array[-1] + array[-2]
+      if new_value < limit
+        array.push(array[-1] + array[-2])
+      else 
+        break
+      end
+    end
+    evenArray = array.find_all{|x| x %2 == 0}
+    return evenArray.sum
+end
+
+
+puts even_fibonacci_sum(10)
+puts even_fibonacci_sum(100)
+
